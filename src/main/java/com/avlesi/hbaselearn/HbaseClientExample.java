7327a041952f19d1e9dcb865bb52c1decd293509
Oleksandr Avlesi <a.avlessi@gmail.com>
2018-04-22 19:52:14 +0200

    initial commit

---
+++ src/main/java/com/avlesi/hbaselearn/HbaseClientExample.java
@@ -0,0 +1,41 @@
+package com.avlesi.hbaselearn;
+
+import com.google.protobuf.ServiceException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+import java.io.IOException;
+
+public class HbaseClientExample {
+
+    public void testConnection() throws ZooKeeperConnectionException {
+        Configuration config = HBaseConfiguration.create();
+
+        String path = this.getClass()
+                .getClassLoader()
+                .getResource("hbase-site.xml")
+                .getPath();
+        config.addResource(new Path(path));
+
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+        } catch (MasterNotRunningException e) {
+            System.out.println("HBase is not running." + e.getMessage());
+            return;
+        }
+
+    }
+
+    public static void main(String [] args) throws IOException, ServiceException {
+
+        System.out.println("hello!");
+
+        HbaseClientExample hbaseClientExample = new HbaseClientExample();
+        hbaseClientExample.testConnection();
+
+    }
+}
